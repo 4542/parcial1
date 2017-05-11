b723cd950b35caff9357bdcf65cf63d1cc9f65fa
4542 <anyers.88@gmail.com>
2017-05-10 22:16:04 -0500

    Parcial 1

---
+++ parcial1/default.php
@@ -0,0 +1,31 @@
+Ana
+
+<!doctype html>
+<html lang="es">
+	<head>
+		<title> Biografia </title>
+		<style type="text/css">
+			body {background: pink;}
+			h1 {color: purple;}
+			p {color: #191970;}
+
+		</style>
+	</head>
+	<body>
+		<h1> Ana </h1>
+		<p>
+			Yo soy una estudiante del instituto <strong>UNITEK</strong> del QUINTO CICLO
+		<!-- este es un comentario-->
+		</p>
+		<div id="comportamiento">
+			click aquí
+		</div>
+
+		<script type="text/javascript">
+			var prueba=document.getElementById("comportamiento");
+			prueba.onclick=function(){alert("Hola");}
+		</script>
+		<br><a href="index.html"> :: Regresar al listado ::</a>
+
+	</body>
+</html>
---
+++ parcial1/scripts.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>Scripts PHP <?php echo date("H:i:s");?></title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h1>Scripts <?php echo "PHP";?></h1>
+	<p>Un sript PHP está contenida entre las etiquetas 
+		<b><code>
+			&lt;&#63;php
+		</code></b>y
+		<b><code>
+			&#63;&gt;
+		</code></b>.
+	</p>
+	<br><a href="index.html"> :: Regresar al listado ::</a>
+</body>
+</html>
